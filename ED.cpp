9a33a022135dd276fa355afaf875d0d3c9633532
chrismadden1 <46973709+chrismadden1@users.noreply.github.com>
2019-07-22 19:19:16 -0400

    Add files via upload

--- ED.cpp
+++ ED.cpp
@@ -4,32 +4,32 @@
 #include "matrix.h"
 #include "xstring.cpp"
 #include "bitvect.cpp"
-#include "tostring.cpp"
 
-void   ED(fsu::String s, size_t m, fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt); 
+
+size_t ED(fsu::String s, size_t m, fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt);
 size_t Min(size_t a, size_t b, size_t c);
 int main(int argc, char*argv[])
 {
 	if(argc < 2)
 	{
 		std::cout << "not enough args\n";
-		return;
+		return -1;
 	}
 	fsu::String str1 = argv[1];
 	fsu::String str2 = argv[2];
-	
-	size_t m = str1.size();
-	size_t n = str2.size();
-	
+
+	size_t m = str1.Size();
+	size_t n = str2.Size();
+
 	fsu::BitVector bvs(m);
 	fsu::BitVector bvt(n);
-		
+
 	size_t ed_length = ED(str1, m, str2, n, bvs, bvt);
 	std::cout << "length of ed: " << ed_length << '\n';
 	bvs.Dump(std::cout);
 	bvt.Dump(std::cout);
-	
-	return;
+
+	return 0;
 }
 size_t ED(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt)
 {
@@ -46,7 +46,7 @@ size_t ED(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, 
 		L[i][0] = i;
 	for(j = 0; j <= m; j++)
 		L[0][j] = j;
-	
+
 	for(i = 1; i <= m; i++)
 	{
 		for(j = 1; j <= n; j++)
@@ -63,7 +63,7 @@ size_t ED(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, 
 						   1 + L[i][j-1],//insert
 						   1 + L[i-1][j-1]);//sub
 			}
-		}		
+		}
 	}
 	return L[m][n];
 }
@@ -75,6 +75,5 @@ size_t Min(size_t a, size_t b, size_t c)
 		val = b;
 	if(c < val)
 		val = c;
-	return val;	
+	return val;
 }
-	
--- LCS.cpp
+++ LCS.cpp
@@ -1,70 +1,74 @@
 #include<iostream>
+#include<cstdlib>
 #include "bitvect.h"
 #include "genalg.h"
 #include "matrix.h"
+#include "xstring.h"
 #include "xstring.cpp"
+#include "tostring.h"
+#include "string.h"
+//#include "tostring.cpp"
 #include "bitvect.cpp"
-#include "tostring.cpp"
 
-void   LCS(fsu::String s, size_t m, fsu::String t, size_t n, fsu::Matrix<size_t>& L); 
-//void   LCS(const char* s, size_t m, const char* t, size_t n, fsu::Matrix<size_t>& L); 
 
-size_t LCS(const char* s, size_t m, const char* t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt); 
-//size_t LCS(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt);
+using namespace fsu;
+void  ls1(fsu::String s, size_t m, fsu::String t, size_t n, fsu::Matrix<size_t>& L);
+//void   LCS(const char* s, size_t m, const char* t, size_t n, fsu::Matrix<size_t>& L);
 
-//namespace fsu
+//size_t LCS(const char* s, size_t m, const char* t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt);
+size_t ls2(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt);
+
 
 int main(int argc, char*argv[])
 {
 	if(argc < 2)
 	{
-		cout << "not enough args\n";
-		return;
+		std::cout << "not enough args\n";
+		return -1;
 	}
 	fsu::String str1 = argv[1];
 	fsu::String str2 = argv[2];
-	
-	size_t m = str1.size();
-	size_t n = str2.size();
-	
-	fsu::BitVector * bvs = new fsu::BitVector (m);
-	fsu::BitVector * bvt = new fsu::BitVector (n);
-		
-	size_t lcs_length = LCS(str1, m, str2, n, bvs, bvt);
-	cout << "length of lcs: " << lcs_length << '\n';
+
+	size_t m = str1.Size();
+	size_t n = str2.Size();
+
+	fsu::BitVector  bvs(m); //= new fsu::BitVector (m);
+	fsu::BitVector  bvt(n); //= new fsu::BitVector (n);
+
+	size_t lcs_length = ls2(str1, m, str2, n, bvs, bvt);
+	std::cout << "length of lcs: " << lcs_length << '\n';
 	bvs.Dump(std::cout);
 	bvt.Dump(std::cout);
-	
-	return;
-}
 
-size_t LCS(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt)
+	return 0;
+}
 //size_t LCS(const char* s, size_t m, const char* t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt)
+size_t ls2(fsu::String s, size_t m,fsu::String t, size_t n, fsu::BitVector& bvs, fsu::BitVector& bvt)
 {
 	fsu::Matrix<size_t> L (m+1,n+1, 0);
-	LCS(s, m, t, n, L);
+	ls1(s, m, t, n, L);
 	bvs.Unset();
 	bvt.Unset();
-	size_t i;
-	size_t j;
-	for(i = m; j = n; i > 0; j > 0)
+	size_t i = m;
+	size_t j = n;
+	while(i > 0 && j > 0)
 	{
 		if(s[i-1] == t[j -1])
 		{
-			bvs.Set(i-1); 
+			bvs.Set(i-1);
 			bvt.Set(j-1);
 			--i;
 			--j;
 		}
-		else if(L[i,j] == L[i-1,j])
+		else if(L[i][j] == L[i-1][j])
 			--i;
 		else
 			--j;
-	}		
+	}
 	return L[m][n];
 }
 
-void   LCS(const char* s, size_t m, const char* t, size_t n, fsu::Matrix<size_t>& L)
+void   ls1(fsu::String s, size_t m, fsu::String t, size_t n, fsu::Matrix<size_t>& L)
 {
 	size_t i;
 	size_t j;
@@ -73,11 +77,9 @@ void   LCS(const char* s, size_t m, const char* t, size_t n, fsu::Matrix<size_t>
 		for(j = 1; j <=n; ++j)
 		{
 			if(s[i-1] == t[j-1])
-				L[i,j] = 1 + L[i-1,j-1];
+				L[i][j] = 1 + L[i-1][j-1];
 			else
-				L[i,j] = fsu::Max(L[i-1,j], L[i,j-1]);
-			
-		}	
-	}	
+				L[i][j] = fsu::Max(L[i-1][j], L[i][j-1]);
+		}
+	}
 }
-	
